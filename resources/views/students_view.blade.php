3a7567164e8df7a45adbf6537a7908214dde8dc0
Vladimir Vuckovic <vladimir.vuckovic@devtechgroup.com>
2016-09-09 12:19:26 +0200

    nothing important, just practicing some stuff

--- resources/views/students_view.blade.php
+++ resources/views/students_view.blade.php
@@ -6,6 +6,7 @@
         <tr>
             <th>Username</th>
             <th>Password</th>
+            <th>Action</th>
         </tr>
         </thead>
         <tbody>
@@ -14,9 +15,24 @@
                     <tr>
                         <td><a href="{{ action("StudentController@show", [$student->id]) }} {{--{{ url("/student", $student->id) }}--}}">{{ $student->username }}</a></td>
                         <td>{{ $student->password }}</td>
+                        <td><span><a href="{{ /*action('StudentController@edit')*/ }}" onclick="alert()">Edit</a>
+								|
+								<a href="{{ action('StudentController@edit', [$student->id]) }}" onclick="return false">Delete</a></span></td>
                     </tr>
                 @endforeach
             @endif
         </tbody>
     </table>
+    <div class="container">
+        <div class="row">
+            <a href="{{ action('StudentController@create') }}" class="btn btn-default">Create student</a>
+            <a href="{{ action('StudentController@edit') }}" class="btn btn-default">Edit student</a>
+
+        </div>
+    </div>
+    <script>
+        function alert(){
+            confirm("Are you sure you want to delete selected user?")
+        }
+    </script>
 @stop
