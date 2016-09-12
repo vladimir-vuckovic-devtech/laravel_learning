040411e29bf0e27b9bd431008df902466ac9bf6f
Vladimir Vuckovic <vladimir.vuckovic@devtechgroup.com>
2016-09-12 15:43:13 +0200

    Created seeder for student database

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,3 +21,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+$factory->define(App\Student::class, function (Faker\Generator $faker) {
+
+    return [
+        'username' => $faker->name,
+        'password' => str_random(10),
+    ];
+});
