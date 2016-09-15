d4c1e2976d53e799b6b38f2b69adc6a6831e319c
dejan-babic <dejan.babic@devtechgroup.com>
2016-09-16 00:17:25 +0200

    Switched to constructor DI of the eloquent model to bypass mockery static call problem.

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests;
 use App\Student;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 
 class StudentController extends Controller
 {
+
+    /**
+     * @var Student
+     */
+    private $student;
+
+    public function __construct(Student $student)
+    {
+
+        $this->student = $student;
+    }
+
     /**
      * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
+     * @throws \Exception
      */
     public function index()
     {
@@ -21,7 +32,7 @@ class StudentController extends Controller
             throw new \Exception("Table does not exists in the database.");
         }
 
-        $students = Student::all();
+        $students = $this->student->all();
         $data["students"] = $students;
         $data["title"] = "Students";
         return View::make("students_view", $data);
@@ -47,9 +58,8 @@ class StudentController extends Controller
     public function store(Request $request)
     {
         $this->validateFields($request);
-        Student::create($request->toArray());
+        $this->student->create($request->toArray());
         return redirect()->route("student.index");
-        //echo "from store";
     }
 
     /**
@@ -71,7 +81,7 @@ class StudentController extends Controller
      */
     public function edit($id)
     {
-        $student = Student::findOrFail($id);
+        $student = $this->student->findOrFail($id);
         $data['title'] = "Student edit";
         $data['student'] = $student;
         return view("student_edit", $data);
@@ -88,7 +98,7 @@ class StudentController extends Controller
     public function update(Request $request, $id)
     {
         $this->validateFields($request, $id);
-        $student = Student::findOrFail($id);
+        $student = $this->student->findOrFail($id);
         $student->username = $request->username;
         $student->password = $request->password;
         $student->save();
@@ -104,7 +114,7 @@ class StudentController extends Controller
      */
     public function destroy($id)
     {
-        $student = Student::find($id);
+        $student = $this->student->find($id);
         $student->delete();
         return redirect("/student");
         //echo "from destroy";
